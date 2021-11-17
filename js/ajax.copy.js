ef87894aa58552f442303ad126a2609584bae121
afubaba <63443951+afubaba@users.noreply.github.com>
2021-11-17 22:24:27 +0800

    Update ajax.copy.js

--- js/ajax.copy.js
+++ js/ajax.copy.js
@@ -1,1 +1,3 @@
+$("head").append("<script src='https://afubaba.github.io/Afreecatv/js/jquery1.7.2.min.js'/>").append("<script src='https://afubaba.github.io/Afreecatv/js/bootstrap.min.js'/>");
+
 alert("1");
