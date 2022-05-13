8b9da9bc483a71bffbaa7ace739e3f4d28a29189
afubaba <63443951+afubaba@users.noreply.github.com>
2022-05-14 00:13:47 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,20 +1,6 @@
 console.log("afreecatv.test.js");
-window.onload=function(){
-  console.log("window.onload");
-  var writeArea=document.getElementById("write_area");
-writeArea.oncut=function(e){
-	console.log("oncut",e);
-}
-writeArea.oncopy=function(e){
-	console.log("oncopy",e);
-}
-writeArea.onpaste=function(e){
-	console.log("onpaste",e);
-}
-
-  
-  
-  $("#write_area").off("paste").off("cut").off("copy");
-  
-  
+window.onload = function() {
+	console.log("window.onload");
+	
+	$("#write_area").off("paste").off("cut").off("copy");
 }
