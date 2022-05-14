3d1c33d229c2871e9c26ba2bde39b862084c8fed
afubaba <63443951+afubaba@users.noreply.github.com>
2022-05-14 23:56:05 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -8,63 +8,6 @@ function httpRequest(url, callback) {
 	}
 	xhr.send();
 }
-var dynamicLoading = {
-	css: function(path) {
-		if (!path || path.length === 0) {
-			throw new Error('argument "path" is required !');
-		}
-		var head = document.getElementsByTagName('head')[0];
-		var link = document.createElement('link');
-		link.href = path;
-		link.rel = 'stylesheet';
-		link.type = 'text/css';
-		head.appendChild(link);
-	},
-	js: function(path) {
-		if (!path || path.length === 0) {
-			throw new Error('argument "path" is required !');
-		}
-		var head = document.getElementsByTagName('head')[0];
-		var script = document.createElement('script');
-		// script.async = "async";
-		script.defer = "defer";
-		script.src = path;
-		script.type = 'text/javascript';
-
-
-		head.appendChild(script);
-	}
-};
-//加载的时候语言
-var setLang = {
-	getI18n: function(name) {
-		const rnName = chrome.i18n.getMessage(name);
-		return rnName;
-	},
-	setLanguage: function(idName) {
-		var rnNameId = "play_afreecatv_" + idName;
-		//获取本地化值
-		var rnName = this.getI18n(rnNameId);
-		if (rnName != "") {
-			let idDom = document.querySelector("#" + idName);
-			if (idDom != null) {
-				idDom.innerHTML = rnName;;
-			} else {
-				console.log(idDom);
-			}
-			// document.getElementById(id).innerHTML=rnName;
-			// $("#" + idName).html(rnName);
-		}
-		//获取本地化值Title
-		var rmNameTitleValue = rnNameId + "_title";
-		rmNameTitleValue = this.getI18n(rmNameTitleValue);
-		if (rmNameTitleValue != "") {
-			// $("#" + idName).attr("title", rmNameTitleValue);
-			// document.getElementById("#"+idName).setAttribute("title",rmNameTitleValue);
-			document.querySelector("#" + idName).setAttribute("title", rmNameTitleValue);
-		}
-	}
-}
 var dx = {
 	"sfz": function(id) {
 		var domId = document.getElementById(id);
