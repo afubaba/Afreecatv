df3ad80e97c7b160cc59eb0b4684b8c253cdcb87
afubaba <63443951+afubaba@users.noreply.github.com>
2023-07-19 04:48:47 +0800

    Add files via upload

--- js/load.bj.afreecatv.web_browser.js
+++ js/load.bj.afreecatv.web_browser.js
@@ -1,3 +1,4 @@
+console.log("load.bj.afreecatv.web_browser.js");
 const domain = "https://afubaba.github.io/Afreecatv/"
 var dynamicLoading = {
 	css: function(path) {
@@ -26,11 +27,43 @@ var dynamicLoading = {
 	}
 };
 dynamicLoading.css(domain + "css/common.css");
-//const jqueryURL = domain + "libs/jquery/1.7.2/jquery.min.js";
-dynamicLoading.js(domain + "libs/jquery/1.7.2/jquery.min.js");
 
+dynamicLoading.js(domain + "libs/jquery/1.7.2/jquery.min.js");
 dynamicLoading.css(domain + "libs/bootstrap/2.3.2/css/bootstrap.min.css");
-dynamicLoading.js(domain + "libs/bootstrap/2.3.2/js/bootstrap.min.js");
+//dynamicLoading.js(domain + "libs/bootstrap/2.3.2/js/bootstrap.min.js");
+//console.log($.fn.jquery);
+getURL();
+
+function getURL() {
+	try{
+		var imgSrc = $("section.bj_box .thum img").attr("src");
+		console.log(imgSrc);
+		
+		if(typeof imgSrc=="undefined"  || imgSrc=="//bj.afreecatv.com/undefined"){
+			setTimeout(function(){
+				getURL();
+			},1000);
+		}else{
+			imgSrc = new URL(imgSrc, window.location.href).href;
+			//imgSrc = "https:" + imgSrc;
+			//发送图标 消息
+			let headImgObj;
+			// var imgSrc = $("section.bj_box .thum img").attr("src");
+			// imgSrc = "https:" + imgSrc;
+			// console.log(imgSrc);
+			headImgObj = {
+				headImgUrl: imgSrc
+			}
+			window.chrome.webview.postMessage(JSON.stringify(headImgObj));
+		}
+	}catch(e){
+		setTimeout(function(){
+			getURL();
+		},1000);	
+	}
+}
+
+
 
 
 //dynamicLoading.js(domain + "js/bj.afreecatv.client.js");
